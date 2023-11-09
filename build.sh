39c819ae553dbaec7c9f310f46a7a1cc0fcd3b73
Jan Bölsche <regular.gonzales@googlemail.com>
2023-11-09 11:09:57 +0100

    Upgrade tre-compile and use its --no-csp flag

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -e
-npx tre-compile index.js --force  > public/index.html
+npx tre-compile index.js --force --no-csp > public/index.html
 npx tre-compile sw.js --force --no-indexhtmlify > public/sw.js
 cp manifest.json public
 cp icon2.png icon2_depth.jpg public
