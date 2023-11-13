56f6280469f55dbe6f45deff9c357a2c5483aa68
Jan Bölsche <regular.gonzales@googlemail.com>
2023-11-13 12:10:15 +0100

    Use capability url

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 set -e
+CAP=`cat .pwa-url-capability`
 npx tre-compile index.js --force --no-csp > public/index.html
 npx tre-compile sw.js --force --no-indexhtmlify > public/sw.js
-cp manifest.json public
+sed "s/CAPABILITY-URL/$CAP/" manifest.json > public/manifest.json
 cp icon3.png icon2.png icon2_depth.jpg public
 pushd public
 npx pwa-asset-generator ../icon3.png -i index.html -m manifest.json --favicon --background black
--- server/cli.js
+++ server/cli.js
@@ -1,12 +1,15 @@
+const fs = require('fs')
+const {join} = require('path')
 const Routes = require('../../barberini/query-server/routes')
 const http = require('http')
 const conf = require('rc')('pwa-server', {
   port: 8080
 })
 
+const cap = fs.readFileSync(join(__dirname, '..', '.pwa-url-capability'), 'utf8')
 const routes = Routes()
 const handle = require('.')(conf)
-routes.add('/pwa', handle)
+routes.add('/' + cap, handle)
 
 const server = http.createServer(routes.handle)
 server.listen(conf.port)
